5dcac445b9b181fdae6fb273e5774f83eca49ec5
romxero <randy.white.1986@gmail.com>
2012-11-29 18:32:16 -0800

    refactoring

--- epoll_compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-
-#this is done just to test the epoll build
-gcc epoll_test.c -o epoll.bin -lpthread -g
--- error_save.h
+++
@@ -1,9 +0,0 @@
-typedef unsigned int uint 
-
-
-
-void saveErrorLog()
-{
-	//this is where the error stuff plays out
-	//there should be  a file added to this feature
-}
